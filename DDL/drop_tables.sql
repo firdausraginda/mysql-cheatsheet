6fc41dcb198b8e9e72359673ef55903f0c3e7356
firdausraginda <ragindafirdaus01@gmail.com>
2021-12-27 21:36:39 +0700

    remove the additional code on drop_tables.sql

--- DDL/drop_tables.sql
+++ DDL/drop_tables.sql
@@ -3,12 +3,3 @@ DROP TABLE db_agi.info_mata_kuliah ;
 DROP TABLE db_agi.mata_kuliah ;
 DROP TABLE db_agi.mahasiswa ;
 DROP TABLE db_agi.dosen ;
-
-
--- if can't delete table due to foreign key constraint
-DELETE FROM db_agi.mata_kuliah
-WHERE nip is null;
-
--- need to remove foreign key manually from dbeaver
-
-DELETE FROM db_agi.dosen ;
