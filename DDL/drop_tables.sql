047b3e04939a8c2e32e9bf404077c772ac540f03
firdausraginda <ragindafirdaus01@gmail.com>
2021-12-27 21:32:09 +0700

    add additional drop commands

--- DDL/drop_tables.sql
+++ DDL/drop_tables.sql
@@ -3,3 +3,13 @@ DROP TABLE db_agi.info_mata_kuliah ;
 DROP TABLE db_agi.mata_kuliah ;
 DROP TABLE db_agi.mahasiswa ;
 DROP TABLE db_agi.dosen ;
+
+
+-- if can't delete table due to foreign key constraint
+DELETE FROM db_agi.mata_kuliah
+WHERE nip is null;
+
+ALTER TABLE db_agi.mata_kuliah 
+DROP FOREIGN KEY nip;
+
+DELETE FROM db_agi.dosen  ;
